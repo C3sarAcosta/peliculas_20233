ce100b13c654392efd02081c2e9e9ee72698b044
Cesar Acosta <c3sar.adan@gmail.com>
2023-09-20 11:59:00 -0700

    20-09

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,8 +5,119 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Recibir argumentos de otra pantalla
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'Sin nombre';
     return const Scaffold(
-      body: Text('Details Screen'),
+      body: CustomScrollView(
+        //Widget con comportamientos predefinidos al scroll
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate.fixed(
+              [
+                _PosterAndTitle(),
+                _PosterAndTitle(),
+                _PosterAndTitle(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      //wIDGET PARA QUE SE AGUSTE AL TAMAÑO
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        //Eliminar padding
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: AssetImage('assets/no-image.jpg'),
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: AssetImage('assets/no-image.jpg'),
+              height: 250,
+            ),
+          ),
+          SizedBox(width: 20),
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'movie.title',
+                  style: TextStyle(fontSize: 30),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                Text(
+                  'movie.titleOriganl',
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_outline,
+                      size: 20,
+                      color: Colors.blueAccent,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      'movie.voteAverage',
+                      style: TextStyle(fontSize: 15),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
