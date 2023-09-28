3ff3c3ffae67f8284af24bb620ce1432ff900952
Cesar Acosta <c3sar.adan@gmail.com>
2023-09-28 11:50:28 -0700

    movies provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:peliculas_20233/screens/screens.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+import 'providers/movies_provider.dart';
+
+void main() => runApp(const AppState());
 
 class AppState extends StatelessWidget {
   const AppState({super.key});
---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MoviesProvider extends ChangeNotifier {
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
+  String _language = 'es-MX';
+
+  MoviesProvider() {
+    getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final Map<String, dynamic> decodeData = json.decode(response.body);
+    print(decodeData);
+    print(response.body);
+  }
+}
